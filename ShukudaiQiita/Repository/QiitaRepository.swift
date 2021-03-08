6dd7c4e9727012eefa77a2a135aadc6e0f78dc27
div-antk <51283468+owaii@users.noreply.github.com>
2021-03-08 09:13:24 +0900

    Repositoryの実装

---
+++ ShukudaiQiita/Repository/QiitaRepository.swift
@@ -0,0 +1,26 @@
+//
+//  QiitaRepository.swift
+//  ShukudaiQiita
+//
+//  Created by Takuya Ando on 2021/03/08.
+//
+
+import Moya
+import RxSwift
+
+typealias QiitaAPIRepository = (( _ response: Qiita?, _ error: Swift.Error?) -> Void)
+
+final class QiitaRepository {
+    
+    private static let qiitaApiProvider = MoyaProvider<QiitaApi>()
+    private static let disposeBag: DisposeBag = DisposeBag()
+}
+
+extension QiitaRepository {
+    
+    static func fetchQiita() -> Observable<[Qiita]> {
+        return qiitaApiProvider.rx.request(.getArticles)
+            .map([Qiita].self)
+            .asObservable()
+    }
+}
